1925ea5fde835ff2f034eaffaf3950f6a21bfed4
Divyanshu Rai <160017461+divyanshu2209@users.noreply.github.com>
2024-06-21 14:03:46 +0530

    Update Calculator.js

--- src/Calculator.js
+++ src/Calculator.js
@@ -55,28 +55,22 @@ const Button = styled.button`
   &.equals {
     background-color: #f0a03a;
     color: white;
-    
+    grid-column: span 2;
   }
   &.number {
     background-color: grey;
-  }
-  &.length {
-  grid-column: span 2;
-  }
-  &.nocolor {
-  background-color: #555;
   }
 `;
 
 const calculatorButtons = [
   { label: '(', type: 'function' }, { label: ')', type: 'function' }, { label: 'mc', type: 'memory' }, { label: 'm+', type: 'memory' }, { label: 'm-', type: 'memory' }, { label: 'mr', type: 'memory' },
-  { label: 'C', type: 'clear' }, { label: '+/-', type: 'operator nocolor' }, { label: '%', type: 'operator nocolor' }, { label: '÷', type: 'operator' }, { label: '2nd', type: 'function' }, { label: 'x²', type: 'function' },
+  { label: 'C', type: 'clear' }, { label: '+/-', type: 'operator ' }, { label: '%', type: 'operator ' }, { label: '÷', type: 'operator' }, { label: '2nd', type: 'function' }, { label: 'x²', type: 'function' },
   { label: 'x³', type: 'function' }, { label: 'xʸ', type: 'function' }, { label: 'eˣ', type: 'function' }, { label: '10ˣ', type: 'function' }, { label: '7', type: 'number' }, { label: '8', type: 'number' },
   { label: '9', type: 'number' }, { label: '×', type: 'operator' }, { label: '¹/x', type: 'function' }, { label: '²√x', type: 'function' }, { label: '³√x', type: 'function' }, { label: 'ʸ√x', type: 'function' },
   { label: 'ln', type: 'function' }, { label: 'log₁₀', type: 'function' }, { label: '4', type: 'number' }, { label: '5', type: 'number' }, { label: '6', type: 'number' }, { label: '-', type: 'operator' },
   { label: 'x!', type: 'function' }, { label: 'sin', type: 'function' }, { label: 'cos', type: 'function' }, { label: 'tan', type: 'function' }, { label: 'e', type: 'function' }, { label: 'EE', type: 'function' },
   { label: '1', type: 'number' }, { label: '2', type: 'number' }, { label: '3', type: 'number' }, { label: '+', type: 'operator' }, { label: 'Rad', type: 'function' }, { label: 'sinh', type: 'function' },
-  { label: 'cosh', type: 'function' }, { label: 'tanh', type: 'function' }, { label: 'π', type: 'function' }, { label: 'Rand', type: 'function' }, { label: '0', type: 'number length' }, { label: '.', type: 'number' },
+  { label: 'cosh', type: 'function' }, { label: 'tanh', type: 'function' }, { label: 'π', type: 'function' }, { label: 'Rand', type: 'function' }, { label: '0', type: 'number' }, { label: '.', type: 'number' },
   { label: '=', type: 'equals' },
 ];
 
